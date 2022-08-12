f5796f7f94618e14059834b1bb365c4e11621d6f
AustinFabian <run2austinfabian@gmail.com>
2022-08-12 10:27:46 +0100

    UI seems all  done

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,21 @@
 const nav = document.getElementsByClassName("bar")[0];
 const show = document.getElementsByClassName("hidden")[0];
+const review = document.getElementById("tourReview")
+const formReview = document.getElementById("formReview")
+const contain = document.getElementsByClassName("containment")[0]
 
 $(document).ready(function () {
 	$(nav).click(function () {
 		$(show).toggleClass("activee");
 	});
+
+	$(contain).click(function () {
+		$(show).removeClass("activee");
+	});
+
+	$(review).click(function(){
+		$(formReview).slideToggle("slow");
+	})
 
 	// for owl carousel
 	$('.carousel').owlCarousel({
